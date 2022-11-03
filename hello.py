2e42bfdb8ce9eeab744b542f5bae31ba9f5d7e15
hafsa.saadat <hafsa.saadat@gmail.com>
2022-11-03 14:11:48 +0000

    test

---
+++ hello.py
@@ -0,0 +1,15 @@
+'''
+first_name = "Hafsa"
+last_name = "Saadat"
+print( f"Hi, my name is {first_name}  {last_name}")
+people = ["Hafsa","Fari","shivan","Joe"]
+print(people)
+'''
+text = input("Enter your name")
+print("you entered ", text)
+
+age = int(input("Enter your age"))
+print("You will be", age +5,"years after 5 years")
+
+List = {"hagsa","bettwe"}
+print(type(List))
